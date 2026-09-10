8f33b5bab199bc4426bad71fc9ee47ced41507d6
keyshaaliqa <165625852+keyshaaliqa@users.noreply.github.com>
2026-09-10 19:42:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,200 +38,200 @@ st.divider()
 
 st.subheader("📋 Input Data Mahasiswa")
 
-marital_status = st.number_input("1. Marital status", min_value=0, value=1)
-application_mode = st.number_input("2. Application mode", min_value=0, value=1)
-application_order = st.number_input("3. Application order", min_value=0, value=1)
-course = st.number_input("4. Course", min_value=0, value=1)
+marital_status = st.number_input("Marital status", min_value=0, value=1)
+application_mode = st.number_input("Application mode", min_value=0, value=1)
+application_order = st.number_input("Application order", min_value=0, value=1)
+course = st.number_input("Course", min_value=0, value=1)
 daytime_evening = st.number_input(
-    "5. Daytime/evening attendance",
+    "Daytime/evening attendance",
     min_value=0,
     value=1
 )
 
 previous_qualification = st.number_input(
-    "6. Previous qualification",
+    "Previous qualification",
     min_value=0,
     value=1
 )
 
 previous_qualification_grade = st.number_input(
-    "7. Previous qualification (grade)",
+    "Previous qualification (grade)",
     min_value=0.0,
     value=120.0
 )
 
 nationality = st.number_input(
-    "8. Nacionality",
+    "Nationality",
     min_value=0,
     value=1
 )
 
 mother_qualification = st.number_input(
-    "9. Mother's qualification",
+    "Mother's qualification",
     min_value=0,
     value=1
 )
 
 father_qualification = st.number_input(
-    "10. Father's qualification",
+    "Father's qualification",
     min_value=0,
     value=1
 )
 
 mother_occupation = st.number_input(
-    "11. Mother's occupation",
+    "Mother's occupation",
     min_value=0,
     value=1
 )
 
 father_occupation = st.number_input(
-    "12. Father's occupation",
+    "Father's occupation",
     min_value=0,
     value=1
 )
 
 admission_grade = st.number_input(
-    "13. Admission grade",
+    "Admission grade",
     min_value=0.0,
     value=120.0
 )
 
 displaced = st.number_input(
-    "14. Displaced",
+    "Displaced",
     min_value=0,
     value=0
 )
 
 educational_special_needs = st.number_input(
-    "15. Educational special needs",
+    "Educational special needs",
     min_value=0,
     value=0
 )
 
 debtor = st.number_input(
-    "16. Debtor",
+    "Debtor",
     min_value=0,
     value=0
 )
 
 tuition_fees = st.number_input(
-    "17. Tuition fees up to date",
+    "Tuition fees up to date",
     min_value=0,
     value=1
 )
 
 gender = st.number_input(
-    "18. Gender",
+    "Gender",
     min_value=0,
     value=1
 )
 
 scholarship_holder = st.number_input(
-    "19. Scholarship holder",
+    "Scholarship holder",
     min_value=0,
     value=0
 )
 
 age = st.number_input(
-    "20. Age at enrollment",
+    "Age at enrollment",
     min_value=15,
     max_value=100,
     value=18
 )
 
 international = st.number_input(
-    "21. International",
+    "International",
     min_value=0,
     value=0
 )
 
 # Semester 1
 curricular_1_credited = st.number_input(
-    "22. Curricular units 1st sem (credited)",
+    "Curricular units 1st sem (credited)",
     min_value=0,
     value=0
 )
 
 curricular_1_enrolled = st.number_input(
-    "23. Curricular units 1st sem (enrolled)",
+    "Curricular units 1st sem (enrolled)",
     min_value=0,
     value=0
 )
 
 curricular_1_evaluations = st.number_input(
-    "24. Curricular units 1st sem (evaluations)",
+    "Curricular units 1st sem (evaluations)",
     min_value=0,
     value=0
 )
 
 curricular_1_approved = st.number_input(
-    "25. Curricular units 1st sem (approved)",
+    "Curricular units 1st sem (approved)",
     min_value=0,
     value=0
 )
 
 curricular_1_grade = st.number_input(
-    "26. Curricular units 1st sem (grade)",
+    "Curricular units 1st sem (grade)",
     min_value=0.0,
     value=0.0
 )
 
 curricular_1_without_evaluations = st.number_input(
-    "27. Curricular units 1st sem (without evaluations)",
+    "Curricular units 1st sem (without evaluations)",
     min_value=0,
     value=0
 )
 
 # Semester 2
 curricular_2_credited = st.number_input(
-    "28. Curricular units 2nd sem (credited)",
+    "Curricular units 2nd sem (credited)",
     min_value=0,
     value=0
 )
 
 curricular_2_enrolled = st.number_input(
-    "29. Curricular units 2nd sem (enrolled)",
+    "Curricular units 2nd sem (enrolled)",
     min_value=0,
     value=0
 )
 
 curricular_2_evaluations = st.number_input(
-    "30. Curricular units 2nd sem (evaluations)",
+    "Curricular units 2nd sem (evaluations)",
     min_value=0,
     value=0
 )
 
 curricular_2_approved = st.number_input(
-    "31. Curricular units 2nd sem (approved)",
+    "Curricular units 2nd sem (approved)",
     min_value=0,
     value=0
 )
 
 curricular_2_grade = st.number_input(
-    "32. Curricular units 2nd sem (grade)",
+    "Curricular units 2nd sem (grade)",
     min_value=0.0,
     value=0.0
 )
 
 curricular_2_without_evaluations = st.number_input(
-    "33. Curricular units 2nd sem (without evaluations)",
+    "Curricular units 2nd sem (without evaluations)",
     min_value=0,
     value=0
 )
 
 unemployment_rate = st.number_input(
-    "34. Unemployment rate",
+    "Unemployment rate",
     min_value=0.0,
     value=10.0
 )
 
 inflation_rate = st.number_input(
-    "35. Inflation rate",
+    "Inflation rate",
     value=1.0
 )
 
 gdp = st.number_input(
-    "36. GDP",
+    "GDP",
     value=1.0
 )
 
